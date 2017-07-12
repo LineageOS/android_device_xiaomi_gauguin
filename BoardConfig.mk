ef7d669f79f808d7deb92eaaa904b08822c04ce4
padarshr <padarshr@codeaurora.org>
2017-07-12 17:32:34 +0530

    gauguin: Add device compatibility matrix.
    Add device compatibility matrix to common project and
    targets can point the build variable DEVICE_MATRIX_FILE to
    this file to build device compatibility matrix.
    
    Change-Id: If8e75c5ee8de0ef50d6937ffbd6453fb3f5eb648

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,6 +47,9 @@ TARGET_BOARD_PLATFORM := lito
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
 
+# HIDL
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_gauguin
 TARGET_RECOVERY_DEVICE_MODULES := libinit_gauguin
