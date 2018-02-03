6802375418fef5df1254f142f19f0f2a2edf5ecb
Wei Wang <wvw@google.com>
2018-02-02 21:32:05 -0800

    gauguin: Remove QTI perfd
    Bug: 69270928
    Test: Build and boot device and test camera
    Change-Id: I87b283206f462fb5c8ec9cdd303ae2934fe9bfc4

--- device.mk
+++ device.mk
@@ -362,9 +362,6 @@ PRODUCT_PACKAGES += \
     libcodec2_vndk.vendor
 
 # Perf
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.perf@2.2.vendor
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
 
