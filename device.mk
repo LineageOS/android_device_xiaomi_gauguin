a1ef3545bfb34551d49aee4e70860aa3a63c68ee
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-07-14 18:57:17 +0200

    gauguin: Build common Trust HAL
    Change-Id: I73a24535534a696ae42cdda3195c60aa2e50ff33

--- device.mk
+++ device.mk
@@ -361,6 +361,10 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     libtinyxml2
 
+# Trust HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.trust@1.0-service
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.2-service-qti
