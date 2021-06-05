82e52e6b5d18b86d1667861cdf523041efcae051
TH779 <i@779.moe>
2021-06-05 22:08:48 +0800

    gauguin: Switch to prebuilt audio primary HAL
    Signed-off-by: TH779 <i@779.moe>
    Change-Id: I5130728eb60f38dd9ddde2eebf576eca257eb66f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,6 +24,9 @@ TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a76
 # Assert
 TARGET_OTA_ASSERT_DEVICE := gauguin,gauguinpro,gauguininpro
 
+# Build
+BUILD_BROKEN_DUP_RULES := true
+
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := gauguin
 TARGET_NO_BOOTLOADER := true
