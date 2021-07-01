4eaa3d8f1e15c2927406c55ee51517f3e97da8cb
xNombre <kartapolska@gmail.com>
2021-07-01 11:33:48 +0200

    gauguin: Disable serial console
    Nobody uses it on production.
    
    Signed-off-by: xNombre <kartapolska@gmail.com>
    Change-Id: Idb8d5ceb7eb7b0fe59fc080f55ab3f119fd5b3fc

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,9 +32,7 @@ TARGET_NO_BOOTLOADER := true
 
 # Kernel
 BOARD_KERNEL_CMDLINE := \
-    console=ttyMSM0,115200,n8 \
     androidboot.hardware=qcom \
-    androidboot.console=ttyMSM0 \
     androidboot.memcg=1 \
     lpm_levels.sleep_disabled=1 \
     video=vfb:640x400,bpp=32,memsize=3072000 \
