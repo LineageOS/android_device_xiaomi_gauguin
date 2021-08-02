9fd5763085faa03e52bb3e91948748426a9172f7
Alexander Koskovich <zvnexus@outlook.com>
2021-08-01 19:40:24 -0700

    gauguin: Pick up USB HAL product makefile.
    Change-Id: Ieb44da5e993590bd1b13dfcee0b2f77c1cb85d79

--- device.mk
+++ device.mk
@@ -478,8 +478,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
 # USB
-PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-qti
+$(call inherit-product, vendor/qcom/opensource/usb/vendor_product.mk)
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
