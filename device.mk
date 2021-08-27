6b83c13945ff1091521e41d3ea7f130f750af19f
TH779 <i@779.moe>
2021-08-27 16:58:11 +0800

    gauguin: Build fastbootd
    Signed-off-by: TH779 <i@779.moe>
    Change-Id: Ib6384c68f326ce375c6329ff748bba5e660f8eec

--- device.mk
+++ device.mk
@@ -193,6 +193,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.xiaomi_gauguin
 
+# Fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
 # GPS
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/etc/apdr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/apdr.conf \
