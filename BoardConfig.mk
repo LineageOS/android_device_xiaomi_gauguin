a5e3771d7f94d1500cb6efe0972ddd015cbd0b6e
ZVNexus <ZVNexus@outlook.com>
2021-11-13 23:59:01 +0100

    gauguin: Remove virtual framebuffer
     * It's largely for testing purposes and it consumes
       a large amount of memory. If your hardware doesn't
       have graphics then it can be useful, which isn't the
       case for shipped devices.
    
    Change-Id: I2348718ad3afba846498572161fa7e83dd8b7ce9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,7 +34,6 @@ TARGET_NO_BOOTLOADER := true
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom \
 			androidboot.memcg=1 \
 			lpm_levels.sleep_disabled=1 \
-			video=vfb:640x400,bpp=32,memsize=3072000 \
 			msm_rtb.filter=0x237 \
 			service_locator.enable=1 \
 			androidboot.usbcontroller=a600000.dwc3 \
