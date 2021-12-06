38667dc8098d929fb520cbe1e8d888c24ac055be
baolong24 <1667375308@qq.com>
2021-12-06 22:23:30 +0800

    gauguin: Switch to prebuilt display composer
     * As source build display composer are braking display refresh rate.
    
    Signed-off-by: baolong24 <1667375308@qq.com>
    Change-Id: Ic7fd66d1c6cc988a6c6879977891baa17e9ba2f9

--- device.mk
+++ device.mk
@@ -180,7 +180,6 @@ PRODUCT_PACKAGES += \
     vendor.display.config@2.0 \
     vendor.display.config@2.0.vendor \
     vendor.qti.hardware.display.allocator-service \
-    vendor.qti.hardware.display.composer-service \
     vendor.qti.hardware.display.mapper@1.1.vendor \
     vendor.qti.hardware.display.mapper@2.0.vendor \
     vendor.qti.hardware.display.mapper@3.0.vendor \
