kihhjy/android_device_xiaomi_gauguin-oss

gauguin: light: Do not turn on breathing for unlit hardware states
Change-Id: I093fb2fb81012a473eda232d3c3407d0e0791907

light/Light.cpp
@@ -95,6 +95,11 @@ static void handleNotification(const LightState& state) {
 
     /* Disable breathing or blinking */
     set(BLUE_LED BREATH, 0);
+    set(BLUE_LED BRIGHTNESS, 0);
+
+    if (!blueBrightness) {
+        return;
+    }
 
     switch (state.flashMode) {
         case Flash::HARDWARE:

LineageOS/android_device_xiaomi_gauguin

light/Light.cpp
@@ -95,6 +95,11 @@ static void handleNotification(const LightState& state) {
 
     /* Disable breathing or blinking */
     set(WHITE_LED BREATH, 0);
+    set(WHITE_LED BRIGHTNESS, 0);
+
+    if (!whiteBrightness) {
+        return;
+    }
 
     switch (state.flashMode) {
         case Flash::HARDWARE: