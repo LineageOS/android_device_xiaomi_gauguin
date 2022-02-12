2145420af7c2fe6e3d71ea24b010931f8d8bb2da
LynnrinChan <wuxiaofei31@gmail.com>
2022-02-12 19:55:48 +0800

    Revert "gauguin: Compile libldacBT_bco.so"
     * [Lynnrin]: This library doesn't exist.
    
    This reverts commit 1f6e79e9e432ddc8b0ada25739b6a5b6d408a211.
    
    Change-Id: Ice877484bfb5bacedc2ba8d5ac2da641044208df

--- device.mk
+++ device.mk
@@ -142,8 +142,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
-    vendor.qti.hardware.btconfigstore@2.0.vendor \
-    libldacBT_bco
+    vendor.qti.hardware.btconfigstore@2.0.vendor
 
 # Camera
 PRODUCT_PACKAGES += \
