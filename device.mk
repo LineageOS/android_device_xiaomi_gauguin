d012e15d08c3505d049fa8cc1a7ebbfe3b843579
Alexander Koskovich <akoskovich@pm.me>
2022-02-15 00:28:21 +0800

    gauguin: Update telephony package list to LA.QSSI.12.0.r1-05600-qssi.0
    Change-Id: Id6fdabd9d5e0df6d23a982e0e39e06a8acff67ef

--- device.mk
+++ device.mk
@@ -415,6 +415,10 @@ PRODUCT_PACKAGES += \
 
 # Telephony
 PRODUCT_PACKAGES += \
+    extphonelib \
+    extphonelib-product \
+    extphonelib.xml \
+    extphonelib_product.xml \
     ims-ext-common \
     ims_ext_common.xml \
     qti-telephony-hidl-wrapper \
@@ -423,6 +427,8 @@ PRODUCT_PACKAGES += \
     qti_telephony_hidl_wrapper_prd.xml \
     qti-telephony-utils \
     qti_telephony_utils.xml \
+    qti-telephony-utils-prd \
+    qti_telephony_utils_prd.xml \
     telephony-ext
 
 PRODUCT_BOOT_JARS += \
