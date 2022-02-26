846660a4ae891d46d622b9659c9415c520ccdfe0
althafvly <althafvly@gmail.com>
2022-02-26 22:16:30 +0530

    gauguin: Set BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE
    Change-Id: I108504e650895ed62dd54f30d6c53f9d16baa5a3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -161,10 +161,10 @@ VENDOR_SECURITY_PATCH := 2023-06-01
 
 # SELinux
 include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
-
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE := true
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
