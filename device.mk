00c29bdb05b1b18a0c29263dc6962b6d67e71e40
LynnrinChan <wuxiaofei31@gmail.com>
2022-07-08 14:23:58 +0800

    gauguin: Add regulatory_info

--- device.mk
+++ device.mk
@@ -21,6 +21,11 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+PRODUCT_PACKAGES += \
+    SettingsOverlayM2007J17C \
+    SettingsOverlayM2007J17G \
+    SettingsOverlayM2007J17I
+
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
