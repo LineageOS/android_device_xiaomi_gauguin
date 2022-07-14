29e1fa0e47eff185eb79351aa6fdaca41fcc8b1d
danascape <danascape@gmail.com>
2022-07-14 10:13:30 -0600

    gauguin: Drop atrace HAL
    * Throws an exception.
    
    Change-Id: I5af8056853058ad29be7debb5c5af5dea83d84a9

--- device.mk
+++ device.mk
@@ -94,10 +94,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     fs_config_files
 
-# Atrace
-PRODUCT_PACKAGES += \
-    android.hardware.atrace@1.0-service
-
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl \
