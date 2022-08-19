3e0d8feea504375f97ad7a5f4f37e913065ef23e
tarsin <yuanqingxiang233@163.com>
2022-08-19 11:03:33 +0000

    gauguin: Make shim for com.mi.node.watermark.so
    Signed-off-by: Kujou Yuko <i@779.moe>
    Change-Id: I47fea33432692a7a76d71db91676cb3a58ac19c7

--- device.mk
+++ device.mk
@@ -435,6 +435,10 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-service \
     libsensorndkbridge
 
+# Shim
+PRODUCT_PACKAGES += \
+    libwatermark_shim
+
 # Telephony
 PRODUCT_PACKAGES += \
     ims-ext-common \
