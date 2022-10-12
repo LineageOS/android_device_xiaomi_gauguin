8343fa7a4a84daabafccbe38f34f547fb3089076
LynnrinChan <lynnrin@lynnrin.moe>
2022-10-13 02:00:24 +0800

    gauguin: Use developer gsi keys
    Change-Id: Ib50cae37895028176bed7c6bc5faa4bc8d535ae5

--- device.mk
+++ device.mk
@@ -8,7 +8,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
