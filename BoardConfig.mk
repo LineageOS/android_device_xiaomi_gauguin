f68f99afe9c75fde36eabb7561ad5d79ab766ddf
Michael Bestas <mkbestas@lineageos.org>
2022-11-14 13:11:09 +0200

    gauguin: Compile the kernel without GCC
    Change-Id: I882c0d8600f88c1bb939c7ee247230ec5e70338a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -49,8 +49,9 @@ BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
 BOARD_RAMDISK_USE_LZ4 := true
 
-TARGET_KERNEL_SOURCE := kernel/xiaomi/gauguin
 TARGET_KERNEL_CONFIG := vendor/lito-perf_defconfig vendor/xiaomi/gauguin.config
+TARGET_KERNEL_NO_GCC := true
+TARGET_KERNEL_SOURCE := kernel/xiaomi/gauguin
 
 # Platform
 TARGET_BOARD_PLATFORM := lito
