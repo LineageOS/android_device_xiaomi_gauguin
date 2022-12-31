4f8d2e3abf2e8b9133caf6457af5d4f3329a57f3
LynnrinChan <lynnrin@lynnrin.moe>
2022-12-31 22:06:38 +0800

    gauguin: Update blobs from V13.0.5.0.SJSCNXM
    Change-Id: Ie9b27c945bc9dcec9a44448800d5ff5ae4630ab4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -160,7 +160,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2022-01-11
+VENDOR_SECURITY_PATCH := 2022-05-01
 
 # SELinux
 include device/qcom/sepolicy_vndr/SEPolicy.mk
