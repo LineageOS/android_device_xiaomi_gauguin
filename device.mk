bc1b70ef017ec9e27e4e2fe456854af6a0fb2c63
LynnrinChan <lynnrin@lynnrin.moe>
2023-01-02 13:35:31 +0800

    gauguin: Drop FM
     * No FM support on MIUI CN
    
    Change-Id: I874fe117fc53c32682615f0246ba468f868b0633

--- device.mk
+++ device.mk
@@ -211,11 +211,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
-# FM
-PRODUCT_PACKAGES += \
-    FM2 \
-    qcom.fmradio
-
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor
