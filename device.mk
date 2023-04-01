a5dd2c195f4b6220e793d63dd83053d8cb2fa267
hridaya <hridaya@pixelos.net>
2023-04-01 11:20:56 +0545

    gauguin: Import powerhint.json
    * From redbull android-13.0.0_r35
    
    Change-Id: If26c96020f1daa3af1db5a1b187b5a9a77584c31

--- device.mk
+++ device.mk
@@ -371,6 +371,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi-libperfmgr
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson
