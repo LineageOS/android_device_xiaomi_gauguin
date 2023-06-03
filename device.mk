a8f6e22a652b470988d9f05912da5af8bdb4ca63
kawaaii <kawaaii@nocturn9x.space>
2023-06-03 18:40:24 +0545

    Revert "gauguin: Add support for misys"
    - This library is used for providing support for MIUI frameworks.
    - Not required on AOSP.
    
    This reverts commit 25848a38718da33fc50d904f3c127d45c867f34b.
    
    Change-Id: I43c5fe243e71c500d070d4faf1f8d37021a4989b

--- device.mk
+++ device.mk
@@ -234,10 +234,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0.vendor \
-    android.hidl.manager@1.0 \
-    android.hidl.manager@1.0.vendor \
-    android.hidl.memory.block@1.0 \
-    android.hidl.memory.block@1.0.vendor \
     libhidltransport \
     libhidltransport.vendor \
     libhwbinder \
