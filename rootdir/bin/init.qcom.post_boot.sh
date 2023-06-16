08ab2683941c03453d0a612374d3896209c5fa81
hridaya <info.hridayaprajapati@gmail.com>
2023-06-16 08:20:05 +0545

    gauguin: post_boot: Drop CAF core control tunings
    - CAF Core Control has been disabled as of kernel commit [1].
    
    [1]: a93d442f41ff21c2cab4112c0a7e112f5206cb96 {"ARM64/configs: gauguin: Disable Qualcomm core control"}
    
    Change-Id: Ib46a7b00454edaef3dd0353ec207e217d8922bb8

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -41,17 +41,6 @@ case "$target" in
     case "$soc_id" in
         "434" | "459" )
 
-        # Core control parameters on silver
-        echo 0 0 0 0 1 1 > /sys/devices/system/cpu/cpu0/core_ctl/not_preferred
-        echo 4 > /sys/devices/system/cpu/cpu0/core_ctl/min_cpus
-        echo 60 > /sys/devices/system/cpu/cpu0/core_ctl/busy_up_thres
-        echo 40 > /sys/devices/system/cpu/cpu0/core_ctl/busy_down_thres
-        echo 100 > /sys/devices/system/cpu/cpu0/core_ctl/offline_delay_ms
-        echo 8 > /sys/devices/system/cpu/cpu0/core_ctl/task_thres
-
-        # Disable Core control on gold
-        echo 0 > /sys/devices/system/cpu/cpu6/core_ctl/enable
-
         # disable unfiltering
         echo 20000000 > /proc/sys/kernel/sched_task_unfilter_period
 
