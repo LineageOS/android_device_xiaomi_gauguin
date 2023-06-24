63df5c7218fb2af96ff390bf8cc0ae4d7b314ed1
hridaya <info.hridayaprajapati@gmail.com>
2023-06-24 19:11:02 +0545

    gauguin: bin: post_boot: Drop platform check
    Change-Id: Ic259d6886112ecbf1ec229fc6d38cb2d602a08fe

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -27,79 +27,58 @@
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
-target=`getprop ro.board.platform`
+# disable unfiltering
+echo 20000000 > /proc/sys/kernel/sched_task_unfilter_period
 
-case "$target" in
-    "lito")
+# Setting b.L scheduler parameters
+# default sched up and down migrate values are 95 and 85
+echo 65 > /proc/sys/kernel/sched_downmigrate
+echo 71 > /proc/sys/kernel/sched_upmigrate
 
-    #Apply settings for lito
-    if [ -f /sys/devices/soc0/soc_id ]; then
-        soc_id=`cat /sys/devices/soc0/soc_id`
-    fi
+# default sched up and down migrate values are 100 and 95
+echo 85 > /proc/sys/kernel/sched_group_downmigrate
+echo 100 > /proc/sys/kernel/sched_group_upmigrate
+echo 1 > /proc/sys/kernel/sched_walt_rotate_big_tasks
+echo 0 > /proc/sys/kernel/sched_coloc_busy_hyst_ns
+echo 0 > /proc/sys/kernel/sched_coloc_busy_hysteresis_enable_cpus
+echo 0 > /proc/sys/kernel/sched_coloc_busy_hyst_max_ms
 
-    #Apply settings for lagoon
-    case "$soc_id" in
-        "434" | "459" )
+# configure governor settings for little cluster
+echo "schedutil" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo 1248000 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/hispeed_freq
+echo 576000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
 
-        # disable unfiltering
-        echo 20000000 > /proc/sys/kernel/sched_task_unfilter_period
+# configure governor settings for big cluster
+echo "schedutil" > /sys/devices/system/cpu/cpu6/cpufreq/scaling_governor
+echo 1248000 > /sys/devices/system/cpu/cpu6/cpufreq/schedutil/hispeed_freq
+echo 652800 > /sys/devices/system/cpu/cpu6/cpufreq/scaling_min_freq
 
-        # Setting b.L scheduler parameters
-        # default sched up and down migrate values are 95 and 85
-        echo 65 > /proc/sys/kernel/sched_downmigrate
-        echo 71 > /proc/sys/kernel/sched_upmigrate
+#colocation v3 settings
+echo 740000 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/rtg_boost_freq
+echo 0 > /sys/devices/system/cpu/cpufreq/policy6/schedutil/rtg_boost_freq
+echo 51 > /proc/sys/kernel/sched_min_task_util_for_boost
 
-        # default sched up and down migrate values are 100 and 95
-        echo 85 > /proc/sys/kernel/sched_group_downmigrate
-        echo 100 > /proc/sys/kernel/sched_group_upmigrate
-        echo 1 > /proc/sys/kernel/sched_walt_rotate_big_tasks
-        echo 0 > /proc/sys/kernel/sched_coloc_busy_hyst_ns
-        echo 0 > /proc/sys/kernel/sched_coloc_busy_hysteresis_enable_cpus
-        echo 0 > /proc/sys/kernel/sched_coloc_busy_hyst_max_ms
+# sched_load_boost as -6 is equivalent to target load as 85. It is per cpu tunable.
+echo -6 >  /sys/devices/system/cpu/cpu6/sched_load_boost
+echo -6 >  /sys/devices/system/cpu/cpu7/sched_load_boost
+echo 85 > /sys/devices/system/cpu/cpu6/cpufreq/schedutil/hispeed_load
 
-        # configure governor settings for little cluster
-        echo "schedutil" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-        echo 1248000 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/hispeed_freq
-        echo 576000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+# Enable conservative pl
+echo 1 > /proc/sys/kernel/sched_conservative_pl
 
-        # configure governor settings for big cluster
-        echo "schedutil" > /sys/devices/system/cpu/cpu6/cpufreq/scaling_governor
-        echo 1248000 > /sys/devices/system/cpu/cpu6/cpufreq/schedutil/hispeed_freq
-        echo 652800 > /sys/devices/system/cpu/cpu6/cpufreq/scaling_min_freq
+# Disable wsf for all targets beacause we are using efk.
+# wsf Range : 1..1000 So set to bare minimum value 1.
+echo 1 > /proc/sys/vm/watermark_scale_factor
 
-        #colocation v3 settings
-        echo 740000 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/rtg_boost_freq
-        echo 0 > /sys/devices/system/cpu/cpufreq/policy6/schedutil/rtg_boost_freq
-        echo 51 > /proc/sys/kernel/sched_min_task_util_for_boost
+# cpuset parameters
+echo 0-5 > /dev/cpuset/background/cpus
+echo 0-5 > /dev/cpuset/system-background/cpus
 
-        # sched_load_boost as -6 is equivalent to target load as 85. It is per cpu tunable.
-        echo -6 >  /sys/devices/system/cpu/cpu6/sched_load_boost
-        echo -6 >  /sys/devices/system/cpu/cpu7/sched_load_boost
-        echo 85 > /sys/devices/system/cpu/cpu6/cpufreq/schedutil/hispeed_load
+# Turn off scheduler boost at the end
+echo 0 > /proc/sys/kernel/sched_boost
 
-        # Enable conservative pl
-        echo 1 > /proc/sys/kernel/sched_conservative_pl
-
-	# Disable wsf for all targets beacause we are using efk.
-	# wsf Range : 1..1000 So set to bare minimum value 1.
-	echo 1 > /proc/sys/vm/watermark_scale_factor
-
-        # cpuset parameters
-        echo 0-5 > /dev/cpuset/background/cpus
-        echo 0-5 > /dev/cpuset/system-background/cpus
-
-        # Turn off scheduler boost at the end
-        echo 0 > /proc/sys/kernel/sched_boost
-
-        # Turn off sleep modes
-        echo 0 > /sys/module/lpm_levels/parameters/sleep_disabled
-      ;;
-    esac
-esac
+# Turn off sleep modes
+echo 0 > /sys/module/lpm_levels/parameters/sleep_disabled
 
 # Post-setup services
-case "$target" in
-    "lito")
-        setprop vendor.post_boot.parsed 1
-    ;;
-esac
+setprop vendor.post_boot.parsed 1
