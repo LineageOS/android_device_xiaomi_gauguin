29334c15fcc0b41c8c92fb605e6b00ac6f0bf9c9
hridaya <info.hridayaprajapati@gmail.com>
2023-06-24 21:31:18 +0545

    gauguin: Import missing NFC configs and copy them properly
    D android.hardware.nfc_snxxx@1.2-service: readConfig; filename is /vendor/etc/libnfc-nxp.conf
    D android.hardware.nfc_snxxx@1.2-service: readNxpRFConfig-Enter..Reading /system/vendor/libnfc-nxp_RF.conf
    E android.hardware.nfc_snxxx@1.2-service: readConfig Cannot open config file /system/vendor/libnfc-nxp_RF.conf
    
    I com.android.nfc: ConfigFile - Parsing file '/vendor/etc/libnfc-nci.conf'
    
    Change-Id: I2e2ace40d1a4789597b5c42f599e02eaf8136409

--- device.mk
+++ device.mk
@@ -329,7 +329,9 @@ PRODUCT_PACKAGES += \
     Tag
 
 PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/nfc/,$(TARGET_COPY_OUT_VENDOR)/etc)
+    $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
+    $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf \
+    $(LOCAL_PATH)/configs/nfc/libnfc-nxp_RF.conf:$(TARGET_COPY_OUT_VENDOR)/libnfc-nxp_RF.conf
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.ese.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_nfc/android.hardware.nfc.ese.xml \
