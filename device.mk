336dfaf8d03a9361dcd89de3ea17ab0e5db87f96
hridaya <info.hridayaprajapati@gmail.com>
2023-06-28 09:00:30 +0545

    gauguin: Build QTI BT Audio HAL 2.0
    Change-Id: Ic6ed88308bfffc8c56ed1e3c8d80927b0e4339ff

--- device.mk
+++ device.mk
@@ -103,6 +103,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth.audio-impl \
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
