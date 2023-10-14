f14c10a064ace1f4df65d9e9fdaba5a1eb7f3535
hridaya <info.hridayaprajapati@gmail.com>
2023-10-14 20:17:22 +0545

    gauguin: Move aux camera packagelist prop to /vendor
    This reverts commit 59e3eff84fcbb735248d4a4f0660debe2b58e54c.
    
    Change-Id: If5f28a953d43454987e6aaa20981b8f95a3829be

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -134,7 +134,6 @@ TARGET_COPY_OUT_VENDOR := vendor
 TARGET_ODM_PROP += $(DEVICE_PATH)/properties/odm.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/properties/product.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/properties/system.prop
-TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/properties/system_ext.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/properties/vendor.prop
 
 # Power
