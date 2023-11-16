3c2678740f1426dc7f346c2fc20655b84f34ea10
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2023-11-16 15:09:10 +0545

    gauguin: Inherit QTI vibrator Makefile
    Change-Id: I2b0e8e42fcc31d8b14e9b8ec8a4d30a97700db8a

--- device.mk
+++ device.mk
@@ -507,11 +507,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
 
 # Vibrator
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service.xiaomi_gauguin
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/vibrator/etc/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+$(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.mk)
 
 # WiFi
 PRODUCT_PACKAGES += \
