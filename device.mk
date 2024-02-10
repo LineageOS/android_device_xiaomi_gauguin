531d795e23241f7bccc71ac6a2a988c553c5dd2e
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2024-02-10 10:26:44 +0545

    gauguin: Move hotword permission to configs/permissions
    Change-Id: I16581691d24a63d44fab112a13bff6c410ee1b3d

--- device.mk
+++ device.mk
@@ -259,7 +259,7 @@ PRODUCT_PACKAGES += \
 
 # HotwordEnrollement
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
 
 # IDC
 PRODUCT_COPY_FILES += \
