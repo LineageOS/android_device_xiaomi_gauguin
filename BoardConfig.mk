3434cb66f6b97824aec77cd809b05525770bb233
Bruno Martins <bgcngm@gmail.com>
2024-06-16 15:14:57 +0100

    gauguin: Inherit from QTI and common Xiaomi FCMs
    Change-Id: I8d4d54bba1b3dff27c11b0034852697b818ee609

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -89,6 +89,8 @@ TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS := false
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 
 DEVICE_MANIFEST_FILE := \
