314cdf9b3806cb0f6c342a8564a5297bc573c441
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2024-06-18 18:58:24 +0545

    gauguin: Drop framework compatibility matrix
    We are switching to common QTI and Xiaomi's FCM in next change.
    
    Change-Id: Ib325b9034e6b0fe9f9a67ba549ccbe4af0ca4340

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -89,7 +89,6 @@ TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS := false
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(DEVICE_PATH)/configs/vintf/framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 
 DEVICE_MANIFEST_FILE := \
