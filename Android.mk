ac2ac9b0ff7a90608473a27524e7d2d1f1c1ee63
Bruno Martins <bgcngm@gmail.com>
2024-06-23 15:42:45 +0100

    gauguin: Declare IMS libs as symlinks during extraction
    Change-Id: Ia1b8097d28dd4888232fe383b830515b1b9bafaa

--- Android.mk
+++ Android.mk
@@ -34,17 +34,6 @@ ALL_DEFAULT_INSTALLED_MODULES += \
 	$(BT_FIRMWARE_MOUNT_POINT) \
 	$(DSP_MOUNT_POINT)
 
-# IMS symlinks
-IMS_LIBS := libimscamera_jni.so libimsmedia_jni.so
-IMS_SYMLINKS := $(addprefix $(TARGET_OUT_SYSTEM_EXT_APPS_PRIVILEGED)/ims/lib/arm64/,$(notdir $(IMS_LIBS)))
-$(IMS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "IMS lib link: $@"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf /system_ext/lib64/$(notdir $@) $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(IMS_SYMLINKS)
-
 # Wifi FW symlinks (WCNSS_qcom_cfg.ini / wlan_mac.bin)
 WIFI_FIRMWARE_SYMLINKS := $(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld
 $(WIFI_FIRMWARE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
