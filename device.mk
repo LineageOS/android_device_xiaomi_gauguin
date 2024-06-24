04cd9360020eadd7380ba78eefdf7d62b24c87f7
Yumi Yukimura <me.cafebabe@gmail.com>
2024-06-25 00:19:16 +0800

    gauguin: Migrate to QTI USB Gadget AIDL HAL
    Change-Id: I6adbca2fab9a3fe0477d17f9786166828b786710

--- device.mk
+++ device.mk
@@ -388,7 +388,9 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.sh
 
 PRODUCT_PACKAGES += \
-    android.hardware.usb-service.qti
+    android.hardware.usb-service.qti \
+    android.hardware.usb.gadget-service.qti \
+    usb_compositions.conf
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
