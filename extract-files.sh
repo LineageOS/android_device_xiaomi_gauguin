307c892033af9e730e98b57b75ccd2b1c9338d6d
Michael Bestas <mkbestas@gmail.com>
2024-09-11 00:04:13 +0300

    gauguin: Shim widevine with libcrypto_shim
    The following change
    https://github.com/google/boringssl/commit/e202e51cb0912f36dafbd2e67cf04d6ec82f3180
    removed symbols that our libs require.
    
    Change-Id: I1325c34ecec3d339dd68c665010ecb36fa2e6a29

--- extract-files.sh
+++ extract-files.sh
@@ -71,6 +71,10 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --add-needed "libpiex_shim.so" "${2}"
             ;;
+        vendor/lib64/libwvhidl.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
+            ;;
         *)
             return 1
             ;;
