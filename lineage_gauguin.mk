f6fc21eb25e56cb83482ecc99f8b555e5f6abc1e
Giovanni Ricca <giovanniricca@proton.me>
2024-11-12 21:27:14 +0100

    gauguin: Drop TARGET_DEVICE and TARGET_PRODUCT
    Reason: We will set it with SKUs props anyway
    
    This reverts partial changes done within commit 0ba3a7e.
    
    Change-Id: I4dbacf4b62c82cc8ef5160cb889a7d8b29548839

--- lineage_gauguin.mk
+++ lineage_gauguin.mk
@@ -22,13 +22,8 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-PRODUCT_SYSTEM_NAME := gauguin_global
-PRODUCT_SYSTEM_DEVICE := gauguin
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="gauguin-user 12 RKQ1.200826.002 V14.0.2.0.SJSMIXM release-keys" \
-    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
-    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
+    PRIVATE_BUILD_DESC="gauguin-user 12 RKQ1.200826.002 V14.0.2.0.SJSMIXM release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := Xiaomi/gauguin_global/gauguin:12/RKQ1.200826.002/V14.0.2.0.SJSMIXM:user/release-keys
