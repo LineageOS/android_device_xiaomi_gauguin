2f70319b0055a91d34d3ffb9c2131947bcd14ab6
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2025-05-06 15:24:50 +0545

    gauguin: switch to common AIDL IR service
    Change-Id: I36c05aaa312969e479896c6e63a26a68cf897afa

--- device.mk
+++ device.mk
@@ -254,7 +254,7 @@ PRODUCT_PACKAGES += \
 
 # IR
 PRODUCT_PACKAGES += \
-    android.hardware.ir-service.xiaomi
+    android.hardware.ir-service.lineage
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
