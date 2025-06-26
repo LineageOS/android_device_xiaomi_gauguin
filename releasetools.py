10c65e41a2790700b611b40b1926d83c3e27bf38
basamaryan <basam.aryan@gmail.com>
2025-06-26 18:53:08 -0400

    gauguin: Reformat releasetools.py
    ruff check --config ../../../tools/extract-utils/ruff.toml *.py --fix
    ruff format --config ../../../tools/extract-utils/ruff.toml *.py
    
    Change-Id: Ife6be491001d4835042fe110b6ea98a02a218107

--- releasetools.py
+++ releasetools.py
@@ -6,23 +6,31 @@
 
 import common
 
+
 def FullOTA_InstallEnd(info):
-  OTA_InstallEnd(info)
-  return
+    OTA_InstallEnd(info)
+    return
+
 
 def IncrementalOTA_InstallEnd(info):
-  OTA_InstallEnd(info)
-  return
+    OTA_InstallEnd(info)
+    return
+
 
 def AddImage(info, basename, dest):
-  name = basename
-  data = info.input_zip.read("IMAGES/" + basename)
-  common.ZipWriteStr(info.output_zip, name, data)
-  info.script.Print("Patching {} image unconditionally...".format(dest.split('/')[-1]))
-  info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
+    name = basename
+    data = info.input_zip.read('IMAGES/' + basename)
+    common.ZipWriteStr(info.output_zip, name, data)
+    info.script.Print(
+        'Patching {} image unconditionally...'.format(dest.split('/')[-1])
+    )
+    info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
+
 
 def OTA_InstallEnd(info):
-  AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
-  AddImage(info, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
-  AddImage(info, "vbmeta_system.img", "/dev/block/bootdevice/by-name/vbmeta_system")
-  return
+    AddImage(info, 'dtbo.img', '/dev/block/bootdevice/by-name/dtbo')
+    AddImage(info, 'vbmeta.img', '/dev/block/bootdevice/by-name/vbmeta')
+    AddImage(
+        info, 'vbmeta_system.img', '/dev/block/bootdevice/by-name/vbmeta_system'
+    )
+    return
