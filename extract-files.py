e4f787122fab7c46453bd8d59e9cd9784120b636
Michael Bestas <mkbestas@lineageos.org>
2025-12-11 13:30:36 +0200

    gauguin: Patch some blobs to depend on libtinyxml2-v34.so
    libtinyxml2 10.1.0 update which landed in BP4A contains ABI
    incompatible changes which cause runtime crashes.
    
    Change-Id: Icc67da5f754b0bb30dce6192b1e520fdf1349ac5

--- extract-files.py
+++ extract-files.py
@@ -76,6 +76,8 @@ blob_fixups: blob_fixups_user_type = {
         .sig_replace('E7 09 00 94', '1F 20 03 D5'),
     'vendor/lib64/libwvhidl.so': blob_fixup()
         .add_needed('libcrypto_shim.so'),
+    'vendor/lib64/libdpps.so': blob_fixup()
+        .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
